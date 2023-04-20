808e3d5cc60753aff8f0279049e156f78e05d432
ArthurWu6688 <935391095@qq.com>
2023-04-20 13:41:50 +0800

    更正的项目名称

---
+++ cmake-build-debug/CMakeFiles/test_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_cpp.dir/main.cpp.obj"
+  "CMakeFiles/test_cpp.dir/main.cpp.obj.d"
+  "libtest_cpp.dll.a"
+  "test_cpp.exe"
+  "test_cpp.exe.manifest"
+  "test_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
